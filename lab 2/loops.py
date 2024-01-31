d4e87cde6596a018685ca82e418c8d3b480a5b5d
Shoqann <arsen.lentaev@gmail.com>
2024-01-31 19:05:42 +0600

    labs

---
+++ lab 2/loops.py
@@ -0,0 +1,21 @@
+#                             While loop
+i=1
+while(i<6):
+    i+=1
+    if i==3:
+        continue
+    if i==4:
+        break
+    print(i)
+#                             For loop
+for i in "Banana":
+    print(i)
+
+0
+for j in "Arsen":
+    if j=="s":
+        continue
+    if j=="e":
+        break
+    print(j)
+
